29059b52595300a2d970b991b406613d105ed59a
ngochoaitn <ngochoaitn@gmail.com>
2017-07-01 09:23:01 +0700

    Đồng bộ lên Github

---
+++ ListView/ListView/Entity/ThongTinNguoi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListView.Entity
+{
+    class ThongTinNguoi
+    {
+        public string HoTen { set; get; }
+        public string QueQuan { set; get; }
+        public int NamSinh { set; get; }
+    }
+}
---
+++ ListView/ListView/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ListView.Entity;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListView
+{
+    public partial class MainPage : ContentPage
+    {
+        ObservableCollection<ThongTinNguoi> _list;
+        public MainPage()
+        {
+            _list = new ObservableCollection<ThongTinNguoi>();
+            InitializeComponent();
+
+            _list.Add(new ThongTinNguoi() {HoTen = "Nông Ngọc Hoài", NamSinh=1994, QueQuan="Phong Huân, Chợ Đồn Bắc Kạn" });
+            _list.Add(new ThongTinNguoi() { HoTen = "Nông Thị Thu Hường", NamSinh = 1996, QueQuan = "Chợ Đồn Bắc Kạn" });
+
+            lv1.ItemsSource = _list;
+        }
+        int _dem = 1;
+        private void btnThemMoi_Clicked(object sender, EventArgs e)
+        {
+            _list.Add(new ThongTinNguoi() { HoTen = string.Format("Họ tên {0}", _dem), QueQuan = string.Format("Xã {0}, huyện {1}, tỉnh {2}", _dem + 1, _dem + 2, _dem + 3) });
+            _dem++;
+        }
+    }
+}
